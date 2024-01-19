d36593d3b9a54650d32773721912f57bc95ef355
Benjamin YAO <benky47@gmail.com>
2024-01-19 00:33:03 +0000

    Config

--- htdocs/conf/conf.php
+++ htdocs/conf/conf.php
@@ -5,32 +5,32 @@
 // Take a look at conf.php.example file for an example of conf.php file
 // and explanations for all possibles parameters.
 //
-$dolibarr_main_url_root='http://localhost/dolibarr-suivie-chantier/htdocs';
-$dolibarr_main_document_root='/Users/BEN/Sites/dolibarr-suivie-chantier/htdocs';
-$dolibarr_main_url_root_alt='/custom';
-$dolibarr_main_document_root_alt='/Users/BEN/Sites/dolibarr-suivie-chantier/htdocs/custom';
-$dolibarr_main_data_root='/Users/BEN/Sites/dolibarr-suivie-chantier/documents';
-$dolibarr_main_db_host='localhost';
-$dolibarr_main_db_port='3306';
-$dolibarr_main_db_name='dolibarr_suivie_chantier';
-$dolibarr_main_db_prefix='llx_';
-$dolibarr_main_db_user='root';
-$dolibarr_main_db_pass='aaaaaaaa';
-$dolibarr_main_db_type='mysqli';
-$dolibarr_main_db_character_set='utf8';
-$dolibarr_main_db_collation='utf8_unicode_ci';
+$dolibarr_main_url_root = 'http://localhost/dolibarr-gestion-chantiers/htdocs';
+$dolibarr_main_document_root = '/Users/BEN/Sites/dolibarr-gestion-chantiers/htdocs';
+$dolibarr_main_url_root_alt = '/custom';
+$dolibarr_main_document_root_alt = '/Users/BEN/Sites/dolibarr-gestion-chantiers/htdocs/custom';
+$dolibarr_main_data_root = '/Users/BEN/Sites/dolibarr-gestion-chantiers/documents';
+$dolibarr_main_db_host = 'localhost';
+$dolibarr_main_db_port = '3306';
+$dolibarr_main_db_name = 'dolibarr_suivie_chantier';
+$dolibarr_main_db_prefix = 'llx_';
+$dolibarr_main_db_user = 'root';
+$dolibarr_main_db_pass = 'aaaaaaaa';
+$dolibarr_main_db_type = 'mysqli';
+$dolibarr_main_db_character_set = 'utf8';
+$dolibarr_main_db_collation = 'utf8_unicode_ci';
 // Authentication settings
-$dolibarr_main_authentication='dolibarr';
+$dolibarr_main_authentication = 'dolibarr';
 
 //$dolibarr_main_demo='autologin,autopass';
 // Security settings
-$dolibarr_main_prod='0';
-$dolibarr_main_force_https='0';
-$dolibarr_main_restrict_os_commands='mysqldump, mysql, pg_dump, pgrestore, clamdscan, clamscan.exe';
-$dolibarr_nocsrfcheck='0';
-$dolibarr_main_instance_unique_id='3ca7624cf35d87a8af19f07c15775905';
-$dolibarr_mailing_limit_sendbyweb='0';
-$dolibarr_mailing_limit_sendbycli='0';
+$dolibarr_main_prod = '0';
+$dolibarr_main_force_https = '0';
+$dolibarr_main_restrict_os_commands = 'mysqldump, mysql, pg_dump, pgrestore, clamdscan, clamscan.exe';
+$dolibarr_nocsrfcheck = '0';
+$dolibarr_main_instance_unique_id = '3ca7624cf35d87a8af19f07c15775905';
+$dolibarr_mailing_limit_sendbyweb = '0';
+$dolibarr_mailing_limit_sendbycli = '0';
 
 //$dolibarr_lib_FPDF_PATH='';
 //$dolibarr_lib_TCPDF_PATH='';
@@ -46,4 +46,4 @@ $dolibarr_mailing_limit_sendbycli='0';
 
 //$dolibarr_font_DOL_DEFAULT_TTF='';
 //$dolibarr_font_DOL_DEFAULT_TTF_BOLD='';
-$dolibarr_main_distrib='standard';
+$dolibarr_main_distrib = 'standard';
